fc739794d0fd6bedd1603040ba4c5880bb0da474
teptimgen <teptimgen@yandex.ru>
2017-01-29 16:25:21 +0300

    Create test.bat

---
+++ Tests/test.bat
@@ -0,0 +1,15 @@
+Maze.exe test1_1_i.txt test1_1_o.txt
+Maze.exe test1_2_i.txt test1_2_o.txt
+Maze.exe test1_3_i.txt test1_3_o.txt
+Maze.exe test1_4_i.txt test1_4_o.txt
+Maze.exe test1_5_i.txt test1_5_o.txt
+Maze.exe test1_6_i.txt test1_6_o.txt
+Maze.exe test1_7_i.txt test1_7_o.txt
+Maze.exe test2_1_i.txt test2_1_o.txt
+Maze.exe test2_2_i.txt test2_2_o.txt
+Maze.exe test2_3_i.txt test2_3_o.txt
+Maze.exe test2_4_i.txt test2_4_o.txt
+Maze.exe test2_5_i.txt test2_5_o.txt
+Maze.exe test2_6_i.txt test2_6_o.txt
+Maze.exe test2_7_i.txt test2_7_o.txt
+pause
